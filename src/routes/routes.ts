44ea696f8f1952b3f94c840ae2ee5e16fcfb539a
archanaamrutha <archanaamrutha2003@gmail.com>
2025-03-28 11:22:42 +0530

    remove health check endpoint from routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,15 +6,6 @@ import { likeRoutes } from "./like-routes.js";
 import { commentRoutes } from "./comments-routes.js";
 export const allroutes = new Hono();
 
-allroutes.get("/health", async (context) => {
-  return context.json(
-    {
-      message: "AllOk",
-    },
-    200
-  );
-});
-
 allroutes.route("/auth", authenticationroutes);
 allroutes.route("/users", usersRoutes);
 allroutes.route("/posts", postRoutes);
